1fabcea31fb5d2311ca776ec6a59e06385e8b36b
John Kitchin <jkitchin@cmu.edu>
2012-10-10 23:00:01 -0400

    add get ENMIN/ENMAX functions

--- POTCAR.py
+++ POTCAR.py
@@ -17,5 +17,21 @@ def get_ZVAL(potcar):
     f.close()
     return float(m.group(1))
 
+def get_ENMAX(potcar):
+    with open(potcar) as f:
+        for line in f:
+            if 'ENMAX' in line:
+                m = re.search('ENMAX\s*=\s*(?P<ENMAX>[0-9]+.[0-9]+);', line)
+                return float(m.groupdict()['ENMAX'])
+
+def get_ENMIN(potcar):
+    with open(potcar) as f:
+        for line in f:
+            if 'ENMIN' in line:
+                m = re.search('ENMIN\s*=\s*(?P<ENMIN>[0-9]+.[0-9]+)\s+eV', line)
+                return float(m.groupdict()['ENMIN'])
+
 if __name__ == '__main__':
     print get_ZVAL('/home/jkitchin/src/vasp/potpaw_PBE/Pd/POTCAR')
+    print get_ENMAX('/home/jkitchin/src/vasp/potpaw_PBE/Pd/POTCAR')
+    print get_ENMIN('/home/jkitchin/src/vasp/potpaw_PBE/Pd/POTCAR')
