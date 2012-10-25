90b86cc2311b7548acfb837b1df7ba7df1653a7c
John Kitchin <jkitchin@cmu.edu>
2012-10-25 09:25:33 -0400

    change parallel executable to mcgaughey version

--- bin/runjasp.py
+++ bin/runjasp.py
@@ -6,7 +6,8 @@ from jasp import *  # need for JASPRC
 #parallel_vasp = '/home/jkitchin/src/vasp/bin/vasp_openmpi_intel_mkl'
 
 # this command works for both serial and MPI
-vaspcmd = '/opt/kitchingroup/vasp-5.2.12/build/bin/vasp-vtst'
+serial_vasp = '/opt/kitchingroup/vasp-5.2.12/build/bin/vasp-vtst'
+parallel_vasp = '/opt/mcgaugheygroup/vasp.5.2.ifort.OpenMPI'
 
 if 'PBS_NODEFILE' in os.environ:
     # we are in the queue. determine if we should run serial or parallel
@@ -15,30 +16,30 @@ if 'PBS_NODEFILE' in os.environ:
     if NPROCS == 1:
         # no question. running in serial.
         print 'NPROCS = ',NPROCS
-        exitcode = os.system(vaspcmd)
+        exitcode = os.system(serial_vasp)
     else:
         if (JASPRC['queue.nodes'] > 1
             or (JASPRC['queue.nodes'] == 1 and
                 JASPRC['multiprocessing.cores_per_process'] is 'None')):
             # vanilla MPI run. multiprocessing does not work on more
             # than one node, and you must specify in JASPRC to use it
-            print 'MPI NPROCS = ',NPROCS
-            parcmd = 'mpirun -np %i %s' % (NPROCS, vaspcmd)
+            print 'vanilla MPI NPROCS = ',NPROCS
+            parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
+            print parcmd
             exitcode = os.system(parcmd)
         else:
             # we need to run an MPI job on cores_per_process
             if JASPRC['multiprocessing.cores_per_process'] == 1:
-                exitcode = os.system(vaspcmd)
+                print 'running serial multiprocessing job'
+                exitcode = os.system(serial_vasp)
             elif JASPRC['multiprocessing.cores_per_process'] > 1:
                 NPROCS = JASPRC['multiprocessing.cores_per_process']
                 print 'Multiprocessing NPROCS = ',NPROCS
-                parcmd = 'mpirun -np %i %s' % (NPROCS, vaspcmd)
+                parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
                 exitcode = os.system(parcmd)
 else:
     # probably running at cmd line, in serial.
-    open('running','w')
-    exitcode = os.system(vaspcmd)
-    if os.path.exists('running'):
-        os.unlink('running')
-    open('done','w')
+    print    'Running vanilla serial job'
+    exitcode = os.system(serial_vasp)
+
 #end
