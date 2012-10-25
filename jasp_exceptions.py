8ab2b63d0690d56fbec3533942d49f82a435428b
John Kitchin <jkitchin@cmu.edu>
2012-10-25 09:26:26 -0400

    add VaspNotFinished

--- jasp_exceptions.py
+++ jasp_exceptions.py
@@ -21,7 +21,10 @@ class VaspRunning(Exception):
     pass
 
 class VaspNotFinished(Exception):
-    pass
+    def __init__(self, message=''):
+        self.message = message
+    def __str__(self):
+        return self.message
 
 class VaspNotConverged(Exception):
     pass
