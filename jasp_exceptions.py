1b185ad2ebc8f60c6cfd061b5eb09c2ac7dccf0b
John Kitchin <jkitchin@cmu.edu>
2013-01-11 09:13:36 -0500

    it looks like i added spaces here. maybe because of pyflakes.

--- jasp_exceptions.py
+++ jasp_exceptions.py
@@ -3,6 +3,7 @@ from exceptions import Exception
 # Jasp Exceptions
 #############################################
 
+
 class VaspQueued(Exception):
     def __init__(self, msg='Queued', cwd=None):
         self.msg = msg
@@ -11,23 +12,30 @@ class VaspQueued(Exception):
     def __str__(self):
         return repr(self.cwd)
 
+
 class VaspSubmitted(Exception):
     def __init__(self, jobid):
         self.jobid = jobid
+
     def __str__(self):
         return repr(self.jobid)
+
 
 class VaspRunning(Exception):
     pass
 
+
 class VaspNotFinished(Exception):
     def __init__(self, message=''):
         self.message = message
+
     def __str__(self):
         return self.message
 
+
 class VaspNotConverged(Exception):
     pass
+
 
 class VaspUnknownState(Exception):
     pass
