10bd697534956233a36a0ad3432a295f343a96ee
John Kitchin <jkitchin@andrew.cmu.edu>
2013-06-28 14:48:45 -0400

    try a link to ase tarball

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setup(name = 'jasp',
       packages=['jasp'],
       scripts=['jasp/bin/runjasp.py','jasp/bin/jaspsum'],
       long_description='''extensions to ase.calculators.vasp. jasp uses modern python patterns and tools.''',
+      dependency_links = ['https://wiki.fysik.dtu.dk/ase-files/python-ase-3.7.1.3184.tar.gz#egg=ase'],
+      
       install_requires=[
-          'https://wiki.fysik.dtu.dk/ase-files/python-ase-3.7.1.3184.tar.gz'
+          'ase'
           #"svn+https://svn.fysik.dtu.dk/projects/ase/trunk", # ase
           #"numpy",
           #"matplotlib",
