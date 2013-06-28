a65fc127324c84693d955b9cbd7a7c8a70b5b5df
John Kitchin <jkitchin@andrew.cmu.edu>
2013-06-28 13:35:39 -0400

    try ase install in setup

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ setup(name = 'jasp',
       scripts=['jasp/bin/runjasp.py','jasp/bin/jaspsum'],
       long_description='''extensions to ase.calculators.vasp. jasp uses modern python patterns and tools.''',
       install_requires=[
-          "ase",
-          "numpy",
-          "matplotlib",
-          "scipy",
-          "webob",
-          "Django",
-          "pyxser",
-          "apsw"
+          "svn+https://svn.fysik.dtu.dk/projects/ase/trunk", # ase
+          #"numpy",
+          #"matplotlib",
+          #"scipy",
+          #"webob",
+          #"Django",
+          #"pyxser",
+          #"apsw"
           ],)
