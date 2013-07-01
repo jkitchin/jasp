76ce698467a981a3a32a0e7870449aac71466846
John Kitchin <jkitchin@cmu.edu>
2013-07-01 10:05:47 -0400

    added some code to make sure the tests run with python setup.py nosetest. the issue is we have to change to the tests directory to make sure relative paths are correct. I grab the path of the module, and construct the tests directory and change to it. the tests run now. I never change back to any directory, but that does not seem to cause any problems  right now.

--- jasp/tests/test-calc-required.py
+++ jasp/tests/test-calc-required.py
@@ -2,17 +2,23 @@ from jasp import *
 from nose import *
 import shutil
 
+import jasp as jaspmod
+jaspdir = os.path.join(os.path.dirname(jaspmod.__file__))
+
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c0', 'c0')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c0')
 
 @with_setup(setup_func, teardown_func)
 def test0():
     "basic setup calculation that requires calculation"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('c0') as calc:
         assert calc.calculation_required(calc.get_atoms(),['energy'])
 
@@ -20,15 +26,18 @@ def test0():
 
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c1', 'c1')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c1')
 
 @with_setup(setup_func, teardown_func)
 def test1():
     "basic setup calculation that does not require calculation"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('c1') as calc:
         assert not calc.calculation_required(calc.get_atoms(),['energy'])
 
@@ -36,15 +45,18 @@ def test1():
 
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c1', 'c1')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c1')
 
 @with_setup(setup_func, teardown_func)
 def test2():
     "basic setup calculation that does changes a parameter and requires calculation"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('c1') as calc:
         calc.set(encut=800)
         assert calc.calculation_required(calc.get_atoms(),['energy'])
@@ -53,15 +65,18 @@ def test2():
 
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c1', 'c1')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c1')
 
 @with_setup(setup_func, teardown_func)
 def test3():
     "basic setup calculation that sets a parameter but should not require a calculation because there is no actual change"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('c1') as calc:
         calc.set(encut=350)
         assert not calc.calculation_required(calc.get_atoms(),['energy'])
@@ -70,17 +85,19 @@ def test3():
 
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c1', 'c1')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c1')
     shutil.rmtree('c2')
 
 @with_setup(setup_func, teardown_func)
 def test4():
     "basic setup calculation that adds constraints so a calculation should be required"
-
+    os.chdir(os.path.join(jaspdir, 'tests'))
     from ase.constraints import FixAtoms
     with jasp('c1') as calc:
         calc.clone('c2')
@@ -94,16 +111,18 @@ def test4():
 
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.copytree('ref/c1', 'c1')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('c1')
 
 @with_setup(setup_func, teardown_func)
 def test5():
     "basic setup calculation that tests if kpts are equal"
-
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('c1',
               kpts=(1,1,1)) as calc:
         print calc.input_params
--- jasp/tests/test-read-ldau_luj.py
+++ jasp/tests/test-read-ldau_luj.py
@@ -5,12 +5,19 @@ import numpy as np
 from nose import *
 import shutil
 
+import jasp as jaspmod
+jaspdir = os.path.join(os.path.dirname(jaspmod.__file__))
+
 def setup_func():
     "set up test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
+    if os.path.isdir('Fe-bcc-U'):
+        shutil.rmtree('Fe-bcc-U')
     shutil.copytree('ref/Fe-bcc-U', 'Fe-bcc-U')
 
 def teardown_func():
     "tear down test fixtures"
+    os.chdir(os.path.join(jaspdir, 'tests'))
     shutil.rmtree('Fe-bcc-U')
 
 @with_setup(setup_func, teardown_func)
@@ -21,6 +28,7 @@ def test():
     a3 = np.array((a/2, a/2, -a/2))
     bulk = Atoms([Atom('Fe', (0, 0, 0), magmom=5)],
                  cell=(a1, a2, a3))
+    os.chdir(os.path.join(jaspdir, 'tests'))
     with jasp('Fe-bcc-U', atoms=bulk,
               debug=logging.DEBUG,
               xc='PBE',
@@ -29,5 +37,5 @@ def test():
               ldau = True,
               ldau_luj={'Fe': {'L':2, 'U':2, 'J':0}},
               lwave=False) as calc:
-
+        # no calculation should be required here.
         assert not calc.calculation_required(calc.get_atoms(),['energy'])
