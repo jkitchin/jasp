2dbea2d5f4b4515e017caae524fb4e0d2bafb7c5
John Kitchin <jkitchin@cmu.edu>
2013-07-01 10:13:34 -0400

    continued work to integrate testing with setuptools

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name = 'jasp',
       version='0.9',
       description='extensions to ase.calculators.vasp',
       url='http://github.com/jkitchin/jasp',
-      maintainer='John Kitchin',
-      maintainer_email='jkitchin@andrew.cmu.edu',
+      author='John Kitchin',
+      author_email='jkitchin@andrew.cmu.edu',
       license='GPL',
       platforms=['linux'],
       packages=['jasp'],
-      scripts=['jasp/bin/runjasp.py','jasp/bin/jaspsum'],
+      scripts=['jasp/bin/runjasp.py','jasp/bin/jaspsum'],      
       test_suite = 'nose.collector',
       long_description='''extensions to ase.calculators.vasp. jasp uses modern python patterns and tools.''',
       #dependency_links = ['https://wiki.fysik.dtu.dk/ase-files/python-ase-3.7.1.3184.tar.gz#egg=ase'],      
-      install_requires=[          
-          #"svn+https://svn.fysik.dtu.dk/projects/ase/trunk", # ase
+      install_requires=[
+          'nose',
           "numpy",
           "matplotlib",
           "scipy",
-          "webob",
-          "Django",
+          #"svn+https://svn.fysik.dtu.dk/projects/ase/trunk", # ase
+          #"webob",
+          #"Django",
           #"pyxser", # https://github.com/dmw/pyxser
           #"apsw" #http://code.google.com/p/apsw/
           ],)
