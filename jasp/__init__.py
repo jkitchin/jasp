ddb7e5afd0a9b905b8c00762de50d1f0142fd190
John Kitchin <jkitchin@andrew.cmu.edu>
2014-12-21 19:03:29 -0500

    added sphinx directive

--- jasp/__init__.py
+++ jasp/__init__.py
@@ -1,14 +1,17 @@
 '''
 Wrapper module ase.calculators.vasp to enable automatic job handling with the Torque queue system.
 
-provides a function Jasp and a context manager jasp.
+provides a function :func:`jasp.Jasp` and a context manager jasp.
 
 Jasp(**kwargs) returns a monkeypatched Vasp calculator.
 
-with jasp(dir,**kwargs) as calc:
-    do stuff
+jasp is a context manager that creates dir necessary, changes into it, does stuff, and changes back to the original directory when done.
 
-is a context manager that creates dir necessary, changes into it, does stuff, and changes back to the original directory when done.
+>>> with jasp(dir, **kwargs) as calc:
+...     do stuff
+
+
+Find the source at http://github.com/jkitchin/jasp and comprehensive examples of jasp usage at http://kitchingroup.cheme.cmu.edu/dft-book.
 '''
 
 from jasp import *
