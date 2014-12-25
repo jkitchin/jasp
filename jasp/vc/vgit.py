75fc6fa38ce95690c6de56d6cf267d6b71456665
John Kitchin <jkitchin@andrew.cmu.edu>
2014-12-25 16:25:20 -0500

    pep8

--- jasp/vc/vgit.py
+++ jasp/vc/vgit.py
@@ -1,12 +1,16 @@
-'''
-module to provide git integration with jasp.
+'''module to provide git integration with jasp.
 
-The intent is to provide a hook function that puts a vasp directory under version control and commits changes to the repository
+The intent is to provide a hook function that puts a vasp directory
+under version control and commits changes to the repository
 
 
 things to do:
-1. figure out how to specify the git repository you want, or have the CWD as the default directory.
-2. figure out how to get more meaningful commit messages, maybe containing diff information or which files were committed.
+
+1. figure out how to specify the git repository you want, or have the
+CWD as the default directory.
+
+2. figure out how to get more meaningful commit messages, maybe
+containing diff information or which files were committed.
 '''
 
 import os, time
@@ -15,6 +19,7 @@ from git import *
 
 CWD = os.getcwd()
 
+
 def under_vc(self):
     '''test if current directory is under version control or not'''
     try:
@@ -22,6 +27,7 @@ def under_vc(self):
         return True
     except InvalidGitRepositoryError:
         return False
+
 
 def commit(self):
     '''add VASP files to a repository and commit them'''
@@ -40,8 +46,6 @@ def commit(self):
 
     message = 'jasp.vc.vgit commit - {0}'.format(time.asctime())
     print index.commit(message)
-
-
 
 if __name__ == '__main__':
     print under_vc(1)
