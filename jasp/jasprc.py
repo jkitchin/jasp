6f2ebfe0297a0325d9ed86315a32656e702aaf03
John Kitchin <jkitchin@cmu.edu>
2015-01-09 20:17:02 -0500

    pep8 cleanup

--- jasp/jasprc.py
+++ jasp/jasprc.py
@@ -22,34 +22,34 @@ also be used to identify the vasp executables used by runjasp.py.
 import os
 
 # default settings
-JASPRC = {'vasp.executable.serial':'/opt/kitchingroup/vasp-5.3.5/bin/vasp-vtst-serial-beef',
-          'vasp.executable.parallel':'/opt/kitchingroup/vasp-5.3.5/bin/vasp-vtst-parallel-beef',
-          'mode':'queue',  # other value is 'run'
-          'queue.command':'qsub',
-          'queue.options':'-joe',
-          'queue.walltime':'168:00:00',
-          'queue.nodes':1,
-          'queue.ppn':1,
-          'queue.mem':'2GB',
-          'queue.jobname':'None',
-          'multiprocessing.cores_per_process':'None'}
+JASPRC = {'vasp.executable.serial': '/opt/kitchingroup/vasp-5.3.5/bin/vasp-vtst-serial-beef',
+          'vasp.executable.parallel': '/opt/kitchingroup/vasp-5.3.5/bin/vasp-vtst-parallel-beef',
+          'mode': 'queue',  # other value is 'run'
+          'queue.command': 'qsub',
+          'queue.options': '-joe',
+          'queue.walltime': '168:00:00',
+          'queue.nodes': 1,
+          'queue.ppn': 1,
+          'queue.mem': '2GB',
+          'queue.jobname': 'None',
+          'multiprocessing.cores_per_process': 'None'}
 
 
 def read_configuration(fname):
     '''reads jasprc configuration from fname'''
     f = open(fname)
     for line in f:
-        line=line.strip()
+        line = line.strip()
 
         if line.startswith('#'):
-            pass # comment
+            pass  # comment
         elif line == '':
             pass
         else:
             if '#' in line:
                 # take the part before the first #
                 line = line.split('#')[0]
-            key,value = line.split('=')
+            key, value = line.split('=')
             JASPRC[key.strip()] = value.strip()
 
 # these are the possible paths to config files, in order of increasing
