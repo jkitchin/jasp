c77319363e28795f478663df39715b6ceb05c47b
John Kitchin <jkitchin@cmu.edu>
2015-01-22 15:15:20 -0500

    simplify runjasp
    remove the use of the vaspe executable  envvars.
    remove support for  multiprocessing. I forgot how it works, and it was confusing.

--- jasp/bin/runjasp.py
+++ jasp/bin/runjasp.py
@@ -1,42 +1,27 @@
 #!/usr/bin/env python
 import os
-from jasp import *  # need for JASPRC
+from jasp.jasprc import JASPRC
 
 # this command works for both serial and MPI
-serial_vasp = (os.environ.get('VASP_SERIAL', None)
-               or JASPRC['vasp.executable.serial'])
+serial_vasp = JASPRC['vasp.executable.serial']
 
-parallel_vasp = (os.environ.get('VASP_PARALLEL', None)
-                 or JASPRC['vasp.executable.parallel'])
+parallel_vasp = JASPRC['vasp.executable.parallel']
 
 if 'PBS_NODEFILE' in os.environ:
     # we are in the queue. determine if we should run serial or parallel
     NPROCS = len(open(os.environ['PBS_NODEFILE']).readlines())
 
     if NPROCS == 1:
-        # no question. running in serial.        
+        # no question. running in serial.
         exitcode = os.system(serial_vasp)
     else:
-        if (JASPRC['queue.nodes'] > 1
-            or (JASPRC['queue.nodes'] == 1 and
-                JASPRC['multiprocessing.cores_per_process'] is 'None')):
-            # vanilla MPI run. multiprocessing does not work on more
-            # than one node, and you must specify in JASPRC to use it
-            
-            parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
-            
-            exitcode = os.system(parcmd)
-        else:
-            # we need to run an MPI job on cores_per_process
-            if JASPRC['multiprocessing.cores_per_process'] == 1:                
-                exitcode = os.system(serial_vasp)
-            elif JASPRC['multiprocessing.cores_per_process'] > 1:
-                NPROCS = JASPRC['multiprocessing.cores_per_process']
-                
-                parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
-                exitcode = os.system(parcmd)
+        # We are running some kind of parallel job. This script only
+        # supports MPI. It used to support multiprocessing, but it was
+        # confusing, so I have taken it out for now.
+        parcmd = 'mpirun -np %i %s' % (NPROCS, parallel_vasp)
+        exitcode = os.system(parcmd)
 else:
     # probably running at cmd line, in serial.
     exitcode = os.system(serial_vasp)
 
-#end
+# end
