4a2aa7e49c5f34ab97e92ccee3ecf9751a28c164
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-09 11:27:10 -0500

    add insert keyword func

--- jasp/vasp-mode/vasp-mode.el
+++ jasp/vasp-mode/vasp-mode.el
@@ -1,3 +1,4 @@
+;;; vasp-mode.el --- Minor-mode to work with VASP calculations
 ;; vasp-mode for syntax highighting of input files
 ;; John Kitchin 3/12/2012
 ;;
@@ -6,6 +7,8 @@
 ;; - figure out how to get help on each keyword, preferrably through info
 
 ;; set these files to open in vasp-mode
+;;; Code:
+
 (add-to-list 'auto-mode-alist '("\\INCAR\\'" . vasp-mode))
 (add-to-list 'auto-mode-alist '("\\POSCAR\\'" . vasp-mode))
 (add-to-list 'auto-mode-alist '("\\KPOINTS\\'" . vasp-mode))
@@ -24,8 +27,7 @@
     "ALGO" "IALGO" "LREAL" "ROPT" "GGA" "VOSKOWN"
     "DIPOL" "AMIX" "BMIX" "WEIMIN" "EBREAK" "DEPER"
     "TIME" "LWAVE" "LCHARG" "LVTOT" "LVHAR" "LELF"
-    "LORBIT" "NPAR" "LSCALAPACK" "LSCALU" "LASYNC"
-    ))
+    "LORBIT" "NPAR" "LSCALAPACK" "LSCALU" "LASYNC"))
 
 (defvar vasp-other-words
   '(; POSCAR
@@ -61,25 +63,23 @@
     "Am" "Cm" "Bk" "Cf" "Es"
     "Fm" "Md" "No" "Lr"))
 
-(setq all-keywords (append vasp-keywords vasp-other-words))
 
-;; auto-generate the regexp to syntax highlight these words
-(defvar vasp-keywords-regexp (regexp-opt all-keywords 'words))
-(defvar vasp-elements-regexp (regexp-opt vasp-element-words 'words))
+(let ((all-keywords (append vasp-keywords vasp-other-words)))
+  ;; auto-generate the regexp to syntax highlight these words
+  (defvar vasp-keywords-regexp (regexp-opt all-keywords 'words))
+  (defvar vasp-elements-regexp (regexp-opt vasp-element-words 'words))
 
-;; list for each font-lock
-(setq vasp-font-lock-keywords
-      `((,vasp-keywords-regexp . font-lock-keyword-face)
-	(,vasp-elements-regexp . font-lock-comment-face)))
+  ;; list for each font-lock
+  (setq vasp-font-lock-keywords
+	`((,vasp-keywords-regexp . font-lock-keyword-face)
+	  (,vasp-elements-regexp . font-lock-comment-face))))
 
-
-;; clear from memory since we dont need these keywords anymore
-;; (setq vasp-keywords nil) ;
 
 ;; the command to comment/uncomment text
 (defun vasp-comment-dwim (arg)
 "Comment or uncomment current line or region in a smart way.
-For detail, see `comment-dwim'."
+For detail, see `comment-dwim'.
+Argument ARG is passed to `comment-dwim'."
    (interactive "*P")
    (require 'newcomment)
    (let ((deactivate-mark nil)
@@ -87,68 +87,85 @@ For detail, see `comment-dwim'."
 	 (comment-end ""))
      (comment-dwim arg)))
 
+
 ;; http://davidavraamides.net/blog/2008/07/22/mode-aware-google-help-in-emacs/
 (defun search-site-url (keyword &optional site inurl lucky)
   "Do a Google search for KEYWORD. Restrict to SITE and INURL, if specified.
-Jump to best match (I Feel Lucky) if LUCKY set.
-"
+Jump to best match (I Feel Lucky) if LUCKY set."
   (concat "http://www.google.com/"
           (format "search?q=%s" (url-hexify-string keyword))
           (if site (format "+site:%s" (url-hexify-string site)))
           (if inurl (format "+inurl:%s" (url-hexify-string inurl)))
           (if lucky "&btnI")))
 
+
 (defun vasp-help ()
   "Open a browser window showing documentation for the word under the point.
 Defaults to a simple keyword search.
-Uses `search-site-url' to do the actual search.
-"
+Uses `search-site-url' to do the actual search."
   (interactive)
   (require 'url)
   (browse-url
    (apply 'search-site-url
           (thing-at-point 'symbol)
-          '("http://cms.mpi.univie.ac.at/vasp/vasp/")
-            )))
+          '("http://cms.mpi.univie.ac.at/vasp/vasp/"))))
+
 
 (defun vasp-ag ()
-  "runs ag on the POSCAR file in current directory"
+  "Run ag on the POSCAR file in current directory."
   (interactive)
   (shell-command "ase-gui POSCAR"))
 
+
 (defun vasp-run ()
-  "runs vasp"
+  "Run VASP in the current directory.
+Assumes the vasp command is \"vasp\"."
   (interactive)
   (start-process-shell-command "vasp" "*scratch*" "vasp"))
 
+
+;; TODO: figure out a way to get options to qsub?
 (defun vasp-queue ()
-  "submits job to a queue system"
+  "Submits job to a queue system.
+Assumes the job is to run vasp in the current directory."
   (interactive)
   (shell-command "echo \"pwd; vasp\" | qsub -d `pwd` -l walltime=12:00"))
 
-(defun vasp-nval (functional chemical-symbol)
-  "return number of valence electrons for the POTCAR
-functional can be lower case. chemical-symbol must be correct"
-  (interactive "sFunctional: \nsChemical Symbol: ")
+
+(defun vasp-nval (chemical-symbol functional )
+  "Return number of valence electrons for a CHEMICAL-SYMBOL.
+The POTCAR path is constructed as $VASP_PP_PATH/potpaw_FUNCTIONAL/CHEMICAL-SYMBOL/POTCAR."
+  (interactive "sChemical Symbol: \nsFunctional: ")
   (shell-command (format "awk 'NR==2{print;exit}' %s"
 			 (format "%s/potpaw_%s/%s/POTCAR"
 				 (getenv "VASP_PP_PATH")
 				 (upcase functional)
 				 chemical-symbol))))
 
+
+(defun vasp-insert-keyword (keyword)
+  "Insert a vasp KEYWORD at point."
+  (interactive (list (ido-completing-read "Keyword: " vasp-keywords)))
+  (insert "%s = " keyword))
+
 ;; define a menu for vasp-mode
 (defvar vasp-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "C-c C-h") 'vasp-help)
+    (define-key map (kbd "C-c C-i") 'vasp-insert-keyword)
     (define-key map (kbd "C-c C-v") 'vasp-ag)
     (define-key map (kbd "C-c C-r") 'vasp-run)
     (define-key map (kbd "C-c C-q") 'vasp-queue)
     map)
-  "Keymap for vasp-mode")
+  "Keymap for vasp-mode.")
 
 (defvar vasp-mode-hook
   nil
-  "*List of functions to call when entering vasp mode.")
+  "List of functions to call when entering vasp mode.")
+
+
+;;; Commentary:
+;;
 
 (require 'easymenu)
 
@@ -176,3 +193,7 @@ functional can be lower case. chemical-symbol must be correct"
 ; completion see: http://www.masteringemacs.org/articles/2012/01/16/pcomplete-context-sensitive-completion-emacs/
 
 (provide 'vasp-mode)
+
+(provide 'vasp-mode)
+
+;;; vasp-mode.el ends here
