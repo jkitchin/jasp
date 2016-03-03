8bd96dc7cc22a3d3cb8a44b316ea176958fece97
John Kitchin <jkitchin@andrew.cmu.edu>
2016-03-03 18:41:22 -0500

    p3 conversions

--- jasp/jasp_atoms.py
+++ jasp/jasp_atoms.py
@@ -1,4 +1,4 @@
-'''Monkey-patched functions for :class:`ase.Atoms`'''
+"""Monkey-patched functions for :class:`ase.Atoms`"""
 
 from ase import Atom, Atoms
 import numpy as np
@@ -7,8 +7,9 @@ import textwrap
 import os
 from POTCAR import get_ZVAL
 
+
 def atoms_equal(self, other):
-    '''Check if this :class:`ase.Atoms` object is identical to
+    """Check if this :class:`ase.Atoms` object is identical to
     :param ase.Atoms other:.
 
     I monkeypatch the ase class because the :func:`ase.io.read` and
@@ -19,7 +20,7 @@ def atoms_equal(self, other):
 
     This function is controversial. I think it solves more problems than
     it creates.
-    '''
+    """
     if other is None:
         return False
 
@@ -29,7 +30,7 @@ def atoms_equal(self, other):
     b = other.arrays
 
     # check if number of atoms have changed.
-    if len(self)!= len(other):
+    if len(self) != len(other):
         return False
 
     if (a['numbers'] != b['numbers']).all():
@@ -86,6 +87,7 @@ def new_repr(self):
 
 Atoms.__repr__ = new_repr
 
+
 def attach_charges(self, fileobj='ACF.dat', displacement=1e-4):
     '''
     Attach the charges from the fileobj to the Atoms.
@@ -94,41 +96,39 @@ def attach_charges(self, fileobj='ACF.dat', displacement=1e-4):
     Does not require the atom positions to be in Bohr and references
     the charge to the ZVAL in the POTCAR
     '''
-    
+
     calc = self.get_calculator()
 
     # Get the sorting and resorting lists
     sort = calc.sort
-    resort = calc.resort    
-    
+    resort = calc.resort
+
     if isinstance(fileobj, str):
         fileobj = open(fileobj)
         f_open = True
-                
+
     # First get a dictionary of ZVALS from the pseudopotentials
     LOP = calc.get_pseudopotentials()
     ppp = os.environ['VASP_PP_PATH']
-    
+
     zval = {}
     for sym, ppath, hash in LOP:
         fullpath = ppp + ppath
         z = get_ZVAL(fullpath)
         zval[sym] = z
 
-    
     # Get sorted symbols and positions according to POSCAR and ACF.dat
     symbols = np.array(self.get_chemical_symbols())[sort]
     positions = self.get_positions()[sort]
 
-    
     charges = []
     sep = '---------------'
-    i = 0 # Counter for the lines
-    k = 0 # Counter of sep
+    i = 0  # Counter for the lines
+    k = 0  # Counter of sep
     assume6columns = False
     for line in fileobj:
-        if line[0] == '\n': # check if there is an empty line in the 
-            i -= 1          # head of ACF.dat file
+        if line[0] == '\n':  # check if there is an empty line in the
+            i -= 1           # head of ACF.dat file
         if i == 0:
             headings = line
             if 'BADER' in headings.split():
@@ -136,11 +136,11 @@ def attach_charges(self, fileobj='ACF.dat', displacement=1e-4):
             elif 'CHARGE' in headings.split():
                 j = headings.split().index('CHARGE')
             else:
-                print('Can\'t find keyword "BADER" or "CHARGE".' \
-                +' Assuming the ACF.dat file has 6 columns.')
+                print('Can\'t find keyword "BADER" or "CHARGE".'
+                      ' Assuming the ACF.dat file has 6 columns.')
                 j = 4
                 assume6columns = True
-        if sep in line: # Stop at last seperator line
+        if sep in line:  # Stop at last seperator line
             if k == 1:
                 break
             k += 1
@@ -152,10 +152,10 @@ def attach_charges(self, fileobj='ACF.dat', displacement=1e-4):
                 if len(words) != 6:
                     raise IOError('Number of columns in ACF file incorrect!\n'
                                   'Check that Bader program version >= 0.25')
-                                
+
             sym = symbols[int(words[0]) - 1]
             charges.append(zval[sym] - float(words[j]))
-            
+
             if displacement is not None:
                 # check if the atom positions match
                 xyz = np.array([float(w) for w in words[1:4]])
@@ -169,11 +169,11 @@ def attach_charges(self, fileobj='ACF.dat', displacement=1e-4):
     charges = np.array(charges)[resort]
     for atom in self:
         atom.charge = charges[atom.index]
-        
+
 Atoms.attach_charges = attach_charges
 
 if __name__ == '__main__':
     from ase.data.molecules import molecule
 
     atoms = molecule('CH3CH2OH')
-    print atoms
+    print(atoms)
