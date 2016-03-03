b9b441dcd5450bd94228c34c819e7797dd985f71
John Kitchin <jkitchin@andrew.cmu.edu>
2016-03-03 18:37:12 -0500

    change quotes to double quotes, do some wrapping.

--- jasp/__init__.py
+++ jasp/__init__.py
@@ -1,18 +1,20 @@
-'''
-Wrapper module ase.calculators.vasp to enable automatic job handling with the Torque queue system.
+"""Wrapper module ase.calculators.vasp to enable automatic job handling with the
+Torque queue system.
 
 provides a function :func:`jasp.Jasp` and a context manager jasp.
 
 Jasp(**kwargs) returns a monkeypatched Vasp calculator.
 
-jasp is a context manager that creates dir necessary, changes into it, does stuff, and changes back to the original directory when done.
+jasp is a context manager that creates dir necessary, changes into it, does
+stuff, and changes back to the original directory when done.
 
 >>> with jasp(dir, **kwargs) as calc:
 ...     do stuff
 
 
-Find the source at http://github.com/jkitchin/jasp and comprehensive examples of jasp usage at http://kitchingroup.cheme.cmu.edu/dft-book.
-'''
+Find the source at http://github.com/jkitchin/jasp and comprehensive examples of
+jasp usage at http://kitchingroup.cheme.cmu.edu/dft-book.
+
+"""
 
 from jasp import *
-
